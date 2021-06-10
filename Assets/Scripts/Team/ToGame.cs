ab97fb6f4afadb3115fbb2be7e8b5b7aebbea4ea
Alexander <scorpion.fitality@gmail.com>
2021-06-10 08:58:00 +0400

    Initial commit

---
+++ Assets/Scripts/Team/ToGame.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ToGame : MonoBehaviour
+{
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        if (anim.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1) SceneManager.LoadScene(1);
+    }
+}
---
+++ Assets/Scripts/UI/Heart.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Heart : MonoBehaviour
+{
+    private Animator anim;
+    private float interval;
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        interval = anim.GetCurrentAnimatorStateInfo(0).length;
+    }
+    private void Update()
+    {
+        if (anim.enabled)
+        {
+            if(interval > 0)
+            {
+                interval -= Time.unscaledDeltaTime;
+            }
+            else
+            {
+                enabled = false;
+                Destroy(gameObject);
+            }
+        }
+    }
+}
