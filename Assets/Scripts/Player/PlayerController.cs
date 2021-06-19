2b18af8fa2c8aa871c261a66de56fc5224f69e4e
Alexander <scorpion.fitality@gmail.com>
2021-06-19 13:17:44 +0400

    Refactoring

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private PlayerShip playerShip;
+
+    private void Start()
+    {
+        playerShip = playerShip == null ? GetComponent<PlayerShip>() : playerShip;
+        if(playerShip == null)
+        {
+            Debug.LogError("Player not set to controller");
+        }
+    }
+
+    private void Update()
+    {
+        if(playerShip != null)
+        {
+            playerShip.MoveToTouchPosition(GetTouchPosition());
+        }
+    }
+
+    private Vector2 GetTouchPosition()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            return Camera.main.ScreenToWorldPoint(touch.position);
+        }
+        else
+        {
+            return playerShip.transform.position;
+        }
+    }
+}
